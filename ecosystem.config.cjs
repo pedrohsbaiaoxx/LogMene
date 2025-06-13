301d4aced24a65e4c1c5b923d19a4d35d7e4229c
pedrohsbaiaoxx <livique.adm@gmail.com>
2025-06-13 10:58:41 -0300

    fix: renomeia ecosystem.config.js para .cjs para forçar CommonJS

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: 'logmene-backend',
+    script: 'server/index.ts',
+    interpreter: 'node',
+    interpreter_args: '--require ts-node/register',
+    env: {
+      NODE_ENV: 'production'
+    },
+    watch: false,
+    max_memory_restart: '1G'
+  }]
+} 
